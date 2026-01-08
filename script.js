7d2571da8809d211f360b2e02b81226cc36169a5
Solomon Nartey <solomonn161@gmail.com>
2026-01-08 00:26:27 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function showRecipe(recipe) {
+    const title = document.getElementById("recipe-title");
+    const text = document.getElementById("recipe-text");
+    const section = document.getElementById("recipe-details");
+
+     
+    section.style.display = "block";
+}
+
+
+// This function runs when a recipe page loads
+//function recipeLoaded(recipeName) {
+  //  alert("Welcome! You are viewing the " + recipeName + " recipe.");
+//}
